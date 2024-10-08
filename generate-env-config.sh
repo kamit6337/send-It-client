afe005aa1f2433dd0a07d9ca04aa2251699894d8
Kamit6337 <kamit6337@gmail.com>
2024-10-08 10:12:17 +0530

    update index.html

--- generate-env-config.sh
+++ generate-env-config.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-cat <<EOF > /usr/share/nginx/html/config.js
-window.env = {
-  VITE_APP_SENTRY_DSN: '$VITE_APP_SENTRY_DSN',
-  VITE_APP_SERVER_URL: '$VITE_APP_SERVER_URL',
-  VITE_APP_CLIENT_URL: '$VITE_APP_CLIENT_URL',
-  VITE_APP_GT4_MEASUREMENT_ID: '$VITE_APP_GT4_MEASUREMENT_ID',
-  VITE_APP_AWS_S3_BUCKET: '$VITE_APP_AWS_S3_BUCKET'
-};
-EOF
+
+# Generate env-config.js from environment variables
+echo "window.env = {" > /usr/share/nginx/html/env-config.js
+
+echo "  VITE_APP_SERVER_URL: \"${VITE_APP_SERVER_URL}\"," >> /usr/share/nginx/html/env-config.js
+echo "  VITE_APP_CLIENT_URL: \"${VITE_APP_CLIENT_URL}\"," >> /usr/share/nginx/html/env-config.js
+echo "  VITE_APP_GT4_MEASUREMENT_ID: \"${VITE_APP_GT4_MEASUREMENT_ID}\"," >> /usr/share/nginx/html/env-config.js
+echo "  VITE_APP_AWS_S3_BUCKET: \"${VITE_APP_AWS_S3_BUCKET}\"," >> /usr/share/nginx/html/env-config.js
+
+echo "}" >> /usr/share/nginx/html/env-config.js
